9e1ef78e3933b9c951cdfca9df6419a7c83d3ae3
lari.rsoares <lari.rsoares@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-06-25 21:12:14 +0000

    inicio do multi camada

--- PerceptronMC/src/ia/perceptron/Perceptron.java
+++ PerceptronMC/src/ia/perceptron/Perceptron.java
@@ -2,6 +2,7 @@ package ia.perceptron;
 
 import java.util.ArrayList;
 import java.util.List;
+import perceptron.Arquivo;
 
 /*
  * To change this template, choose Tools | Templates
@@ -20,4 +21,43 @@ public class Perceptron {
     private double x[] = new double[4];
     
 
+    public void criarCamada(int neoroniosCamadas, int entradasCamada) {
+        camadas.add(new Camada(neoroniosCamadas, entradasCamada));
+    }
+
+    public void teste(){
+
+        int neoroniosCamadas1 = 15;
+        int entradasCamada1 = 5; //4 + o bias
+        int neoroniosCamadas2 = 3;
+        int entradasCamada2 = 4; //3 + o bias
+        int colunas = 8, linhas = 130;
+        String nomeArquivo = "treina.txt";
+
+        double[][] matriz;
+        double[][] matrizDeEntradas;
+        
+
+        criarCamada(neoroniosCamadas1, entradasCamada1);
+        criarCamada(neoroniosCamadas2, entradasCamada2);
+
+        
+        Arquivo arquivo = new Arquivo();
+        matriz = arquivo.lerArquivo(colunas, linhas, nomeArquivo);
+
+        matrizDeEntradas = new double[linhas][entradasCamada1];
+        preencheMatriz(linhas, entradasCamada1, matrizDeEntradas, matriz);
+
+
+        
+  }
+
+    private void preencheMatriz(int linhas, int colunas, double[][] matrizDeEntradas, double[][] matriz){
+        for(int i = 0; i<linhas; i++){
+            for(int j = 0; j<colunas; j++)
+                matrizDeEntradas[i][j] = matriz[i][j];
+            }
+    }
+
+    
 }
---
+++ PerceptronMC/src/ia/perceptron/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ia.perceptron;
+
+import perceptron.Arquivo;
+
+/**
+ *
+ * @author Larissa
+ */
+public class Principal {
+
+
+
+
+    public static void main(String args[]) {
+
+        
+    }
+
+}
