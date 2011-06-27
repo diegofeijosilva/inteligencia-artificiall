6461d9c8e2506b8c65c07487faa0cf1ff2832225
gabriel.ecomp <gabriel.ecomp@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-06-27 21:56:40 +0000

    submetendo implementações para métodos "processar" de Perceptron, Camada e Neuronio. Ainda é preciso fazer os testes.

--- PerceptronMC/src/ia/perceptron/Camada.java
+++ PerceptronMC/src/ia/perceptron/Camada.java
@@ -29,37 +29,61 @@ public class Camada {
 
     private void inicializarNeuronios() {
         for (int i = 0; i < qtdNeuronios; i++) {
-            neuronios.add(new Neuronio(qtdEntradas));
+            neuronios.add(new Neuronio());
         }
     }
 
     private void inicializarMatrizPesos() {
-       
+
         W = new double[qtdNeuronios][qtdEntradas];
         for (int i = 0; i < qtdNeuronios; i++) {
             for (int j = 0; j < qtdEntradas; j++) {
                 W[i][j] = 1 * (double) Math.random();
-                
+
             }
         }
     }
 
-    public int getQtdNeuronios()
-    {
+    protected void processar(double[] x) {
+        ponderarEntradas(x);
+
+        for (int i = 0; i < neuronios.size(); i++) {
+            neuronios.get(i).processar(I[i]);
+        }
+        setSaida();
+    }
+
+    private void ponderarEntradas(double[] entradas) {
+        for (int i = 0; i < qtdNeuronios; i++) {
+            for (int j = 0; j < qtdEntradas; j++) {
+                I[i] += entradas[j] * W[i][j];
+            }
+        }
+    }
+
+    public int getQtdNeuronios() {
         return this.qtdNeuronios;
     }
 
-    public int getQtdEntradas()
-    {
+    public int getQtdEntradas() {
         return this.qtdEntradas;
     }
 
-    public double getPeso(int i, int j)
-    {
+    public double getPeso(int i, int j) {
         return W[i][j];
     }
 
+    public Neuronio getNeuronio(int i) {
+        return neuronios.get(i);
+    }
 
+    public double[] getSaida() {
+        return Y;
+    }
 
-
+    private void setSaida() {
+        for (int i = 0; i < neuronios.size(); i++) {
+            Y[i] = neuronios.get(i).getSaida();
+        }
+    }
 }
