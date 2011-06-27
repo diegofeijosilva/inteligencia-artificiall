c9a70f2bd43a3f038fb2a594c4be5fa98659e3f0
gabriel.ecomp <gabriel.ecomp@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-06-27 19:04:17 +0000

    alterando e adicionando teste para carregamento dos dados nas classes Arquivo.

--- PerceptronMC/test/ia/perceptron/arquivo/ManipuladorArquivoTest.java
+++ PerceptronMC/test/ia/perceptron/arquivo/ManipuladorArquivoTest.java
@@ -41,19 +41,97 @@ public class ManipuladorArquivoTest {
      * Test of carregarDados method, of class ManipuladorArquivo.
      */
     @Test
-    public void testCarregarDados() {
-        System.out.println("carregarDados");
+    public void testCarregarDadosEntradaTreino() {
+        System.out.println("carregarDadosEntradaTreino()");
         ManipuladorArquivo instance = new ManipuladorArquivo();
         instance.carregarDados();
 
-        double[] d = instance.getArquivoTreino().x(0);
-        System.out.println("dados: "+d[0]+" "+d[1]+" "+d[2]+" "+d[3]+" ");
+        double[] obtido1 = instance.getArquivoTreino().x(0);
+        double[] esperado1 = new double[]{0.3841, 0.2021, 0.0, 0.2438};
 
-        assertEquals(0.3841,d[0],0);
-        assertEquals(0.2021,d[1],0);
-        assertEquals(0.0,d[2],0);
-        assertEquals(0.2438,d[3],0);
+        double[] obtido2 = instance.getArquivoTreino().x(129);
+        double[] esperado2 = new double[]{0.3832, 0.2321, 0.0341, 0.2450};
+
+        double[] obtido3 = instance.getArquivoTreino().x(99);
+        double[] esperado3 = new double[]{0.3858, 0.7585, 0.3239, 0.3565};
+
+        double[] obtido4 = instance.getArquivoTreino().x(62);
+        double[] esperado4 = new double[]{0.6880, 0.6004, 0.6602, 0.4320};
         
+
+        assertEquals(esperado1.length,obtido1.length);
+        
+        assertEquals(esperado1[0],obtido1[0],0);
+        assertEquals(esperado1[1],obtido1[1],0);
+        assertEquals(esperado1[2],obtido1[2],0);
+        assertEquals(esperado1[3],obtido1[3],0);
+
+        assertEquals(esperado2.length,obtido2.length);
+
+        assertEquals(esperado2[0],obtido2[0],0);
+        assertEquals(esperado2[1],obtido2[1],0);
+        assertEquals(esperado2[2],obtido2[2],0);
+        assertEquals(esperado2[3],obtido2[3],0);
+
+        assertEquals(esperado3.length,obtido3.length);
+
+        assertEquals(esperado3[0],obtido3[0],0);
+        assertEquals(esperado3[1],obtido3[1],0);
+        assertEquals(esperado3[2],obtido3[2],0);
+        assertEquals(esperado3[3],obtido3[3],0);
+
+        assertEquals(esperado4.length,obtido4.length);
+
+        assertEquals(esperado4[0],obtido4[0],0);
+        assertEquals(esperado4[1],obtido4[1],0);
+        assertEquals(esperado4[2],obtido4[2],0);
+        assertEquals(esperado4[3],obtido4[3],0);
+        
+    }
+
+    @Test
+    public void testCarregarDadosSaidaTreino() {
+        System.out.println("carregarDadosSaidaTreino()");
+        ManipuladorArquivo instance = new ManipuladorArquivo();
+        instance.carregarDados();
+
+        double[] obtido1 = instance.getArquivoTreino().d(0);
+        double[] esperado1 = new double[]{1, 0, 0};
+
+        double[] obtido2 = instance.getArquivoTreino().d(129);
+        double[] esperado2 = new double[]{1, 0, 0};
+
+        double[] obtido3 = instance.getArquivoTreino().d(99);
+        double[] esperado3 = new double[]{0, 1, 0};
+
+        double[] obtido4 = instance.getArquivoTreino().d(62);
+        double[] esperado4 = new double[]{0, 1, 0};
+
+
+        assertEquals(esperado1.length,obtido1.length);
+
+        assertEquals(esperado1[0],obtido1[0],0);
+        assertEquals(esperado1[1],obtido1[1],0);
+        assertEquals(esperado1[2],obtido1[2],0);
+
+        assertEquals(esperado2.length,obtido2.length);
+
+        assertEquals(esperado2[0],obtido2[0],0);
+        assertEquals(esperado2[1],obtido2[1],0);
+        assertEquals(esperado2[2],obtido2[2],0);
+
+        assertEquals(esperado3.length,obtido3.length);
+
+        assertEquals(esperado3[0],obtido3[0],0);
+        assertEquals(esperado3[1],obtido3[1],0);
+        assertEquals(esperado3[2],obtido3[2],0);
+
+        assertEquals(esperado4.length,obtido4.length);
+
+        assertEquals(esperado4[0],obtido4[0],0);
+        assertEquals(esperado4[1],obtido4[1],0);
+        assertEquals(esperado4[2],obtido4[2],0);
+
     }
 
 }
