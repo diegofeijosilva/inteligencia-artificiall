b1e5f9e5db8b217b734d93e3db80a756991fd6e6
gabriel.ecomp <gabriel.ecomp@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-07-03 03:18:29 +0000

    inicio da interface

---
+++ trunk/PerceptronMC/src/ia/perceptron/gui/CellRenderer.java
@@ -0,0 +1,40 @@
+
+package ia.perceptron.gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class CellRenderer extends DefaultTableCellRenderer {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public CellRenderer() {
+        super();
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int row, int column) {
+
+        this.setHorizontalAlignment(CENTER);
+
+        Component component = super.getTableCellRendererComponent(table, value, isSelected,
+                hasFocus, row, column);
+
+        if (column == 0) {
+            component.setFont(component.getFont().deriveFont(Font.BOLD));
+            component.setForeground(Color.BLUE);
+        } else {
+            component.setFont(component.getFont());
+            component.setForeground(Color.BLACK);
+        }
+
+        return component;
+    }
+}
