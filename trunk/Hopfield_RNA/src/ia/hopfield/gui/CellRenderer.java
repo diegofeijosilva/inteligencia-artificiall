ad690c9791217a7e80eba8ab5e0c8cdac16c0d94
gabriel.ecomp <gabriel.ecomp@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-07-08 05:09:06 +0000

    refatorando

--- trunk/Hopfield_RNA/src/ia/hopfield/gui/CellRenderer.java
+++ trunk/Hopfield_RNA/src/ia/hopfield/gui/CellRenderer.java
@@ -27,26 +27,6 @@ public class CellRenderer extends DefaultTableCellRenderer {
         Component component = super.getTableCellRendererComponent(table, value, isSelected,
                 hasFocus, row, column);
 
-//        if (column == 0 || (column >= 8 && column <= 10)) {
-//            component.setFont(component.getFont().deriveFont(Font.BOLD));
-//            component.setForeground(Color.BLUE);
-//        } else {
-//            component.setFont(component.getFont());
-//            component.setForeground(Color.BLACK);
-//        }
-//
-//        if (column == 14) {
-//            component.setFont(component.getFont().deriveFont(Font.BOLD));
-//            if (value == "OK") {
-//                //component.setForeground(PerceptronGUI.verde);
-//            } else if (value == "ERRO") {
-//                component.setForeground(Color.RED);
-//            }
-//        } else {
-//            component.setFont(component.getFont());
-//            component.setForeground(Color.BLACK);
-//        }
-
         if (value == "#") {
             component.setBackground(Color.blue);
             value = new String("");
@@ -54,7 +34,6 @@ public class CellRenderer extends DefaultTableCellRenderer {
         else {
             component.setBackground(Color.white);
         }
-
 
         return component;
     }
