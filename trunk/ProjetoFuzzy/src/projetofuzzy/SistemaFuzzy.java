d2417c2b8af6258a1c02e1cec801da88a69e9e19
lari.rsoares <lari.rsoares@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-07-29 14:11:31 +0000

    Ínicio do Sistema Fuzzy

---
+++ trunk/ProjetoFuzzy/src/projetofuzzy/SistemaFuzzy.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projetofuzzy;
+
+/**
+ *
+ * @author Larissa
+ */
+public class SistemaFuzzy {
+
+
+
+    public void fuzificar()
+    {
+
+    }
+
+    public void desfuzificar()
+    {
+        centroDeArea();
+    }
+
+    public void centroDeArea(){
+
+    }
+
+}
