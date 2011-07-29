b19626ab368b4b3f24de1debf73df9371996144d
lari.rsoares <lari.rsoares@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-07-29 14:28:27 +0000

    mais coisa

--- trunk/ProjetoFuzzy/src/projetofuzzy/VariavelLinguistica.java
+++ trunk/ProjetoFuzzy/src/projetofuzzy/VariavelLinguistica.java
@@ -5,10 +5,28 @@
 
 package projetofuzzy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Larissa
  */
 class VariavelLinguistica {
+    
+    public static final int QTD_CONJUNTOS_FUZZY_DEFAULT = 3;
+
+    private int qtdConjuntos;
+    List<ConjuntoFuzzy> conjuntos = new ArrayList<ConjuntoFuzzy>();
+
+    
+
+    public VariavelLinguistica(int Conj){
+        qtdConjuntos = Conj;
+    }
+
+    public VariavelLinguistica(){
+        qtdConjuntos = QTD_CONJUNTOS_FUZZY_DEFAULT;
+    }
 
 }
