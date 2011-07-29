e28ea77753bf5b5960c267b0996c6904390ab9d9
lari.rsoares <lari.rsoares@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-07-29 14:28:27 +0000

    mais coisa

--- ProjetoFuzzy/src/projetofuzzy/SistemaFuzzy.java
+++ ProjetoFuzzy/src/projetofuzzy/SistemaFuzzy.java
@@ -5,17 +5,32 @@
 
 package projetofuzzy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Larissa
  */
 public class SistemaFuzzy {
 
+    public static final int QTD_VARIAVEIS_ENTRADA_DEFAULT = 2;
+    public static final int QTD_VARIAVEIS_SAIDA_DEFAULT = 1;
 
+    List<VariavelLinguistica> variaveisLinguisticas = new ArrayList<VariavelLinguistica>();
+    private int qtdVarEntradas;
+    private int qtdVarSaida;
 
-    public void fuzificar()
+    public void fuzificar(int varEntradas, int varSaida)
     {
+        qtdVarEntradas = varEntradas;
+        qtdVarSaida = varSaida;
+    }
 
+      public void fuzificar()
+    {
+        qtdVarEntradas = QTD_VARIAVEIS_ENTRADA_DEFAULT;
+        qtdVarSaida = QTD_VARIAVEIS_SAIDA_DEFAULT;
     }
 
     public void desfuzificar()
