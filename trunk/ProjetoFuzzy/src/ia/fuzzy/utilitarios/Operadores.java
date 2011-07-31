0d2113782ca25aac880238ecb2b5ed0eb639373c
lari.rsoares <lari.rsoares@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-07-31 22:58:17 +0000

    feito o operador MaxMin tendo como saida uma matriz 500X500

--- trunk/ProjetoFuzzy/src/ia/fuzzy/utilitarios/Operadores.java
+++ trunk/ProjetoFuzzy/src/ia/fuzzy/utilitarios/Operadores.java
@@ -5,6 +5,9 @@
 
 package ia.fuzzy.utilitarios;
 
+import Jama.Matrix;
+import ia.fuzzy.SistemaFuzzy;
+
 /**
  *
  * @author Larissa
@@ -12,8 +15,46 @@ package ia.fuzzy.utilitarios;
 public class Operadores {
 
 
+    //metodo constrói uma matriz resultante de 500X500
+    public void composicaoMaxMin(double[][] matrizTemperatura, double[][] matrizVolume){
 
-    public void composicaoMaxMin(){
+        double[][] matrizMaxMin = new double[SistemaFuzzy.DISCRETIZACAO_DEFAULT][SistemaFuzzy.DISCRETIZACAO_DEFAULT];
+        double min[] = new double[SistemaFuzzy.QTD_VALORES_LINGUISTICOS_DEFAULT];
+        double max;
+        int linhaMatrizMaxMin = 0;
+
+        for(int i=0; i<SistemaFuzzy.DISCRETIZACAO_DEFAULT; i++){
+            for(int j=0; j<SistemaFuzzy.QTD_VALORES_LINGUISTICOS_DEFAULT; j++){
+                min[j] = minimo(matrizTemperatura[linhaMatrizMaxMin][j], matrizVolume[i][j]);
+            }            
+            max = maximo(min);          
+            matrizMaxMin[linhaMatrizMaxMin][i] = max;
+
+            if(i == 499){
+                i = 0;
+                linhaMatrizMaxMin++;
+            }
+            if(linhaMatrizMaxMin==499){
+                break;
+            }
+        }
+    }
+
+    public double minimo(double x1, double x2){
+        if(x1<=x2)
+            return x1;
+        else if(x1>x2)
+            return x2;
+        return 2;
+    }
+     public double maximo(double[] min){
+        double aux = min[0];
+        for(int i = 1; i <min.length; i++){
+            if(min[i] > aux){
+                aux = min[i];
+            }
+        }
+        return aux;
 
     }
 
