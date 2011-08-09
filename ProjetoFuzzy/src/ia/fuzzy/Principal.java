db30d7bc741a16d0d868758d1ac76e10aedaea59
gabriel.ecomp <gabriel.ecomp@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-08-09 13:58:57 +0000

    começando interface

--- ProjetoFuzzy/src/ia/fuzzy/Principal.java
+++ ProjetoFuzzy/src/ia/fuzzy/Principal.java
@@ -14,52 +14,52 @@ public class Principal {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-
-       double temperaturaEntrada = 965;
-       double volumeEntrada = 11;
-       System.out.println("Temperatura: " + temperaturaEntrada + " Volume: " + volumeEntrada);
-       SistemaFuzzy fuzzy = new SistemaFuzzy();
-       fuzzy.criarVariavelLinguistica("Temperatura", 800, 1200);
-       fuzzy.criarVariavelLinguistica("Volume", 2, 12);
-       fuzzy.criarVariavelLinguistica("Pressao", 4, 12);
-       fuzzy.executarMecanismoInferencia(temperaturaEntrada, volumeEntrada);
-
-       temperaturaEntrada = 920;
-       volumeEntrada = 7.6;
-       System.out.println("\nTemperatura: " + temperaturaEntrada + " Volume: " + volumeEntrada);
-       fuzzy = new SistemaFuzzy();
-       fuzzy.criarVariavelLinguistica("Temperatura", 800, 1200);
-       fuzzy.criarVariavelLinguistica("Volume", 2, 12);
-       fuzzy.criarVariavelLinguistica("Pressao", 4, 12);
-       fuzzy.executarMecanismoInferencia(temperaturaEntrada, volumeEntrada);
-
-       temperaturaEntrada = 1050;
-       volumeEntrada = 6.3;
-       System.out.println("\nTemperatura: " + temperaturaEntrada + " Volume: " + volumeEntrada);
-       fuzzy = new SistemaFuzzy();
-       fuzzy.criarVariavelLinguistica("Temperatura", 800, 1200);
-       fuzzy.criarVariavelLinguistica("Volume", 2, 12);
-       fuzzy.criarVariavelLinguistica("Pressao", 4, 12);
-       fuzzy.executarMecanismoInferencia(temperaturaEntrada, volumeEntrada);
-
-       temperaturaEntrada = 843;
-       volumeEntrada = 8.6;
-       System.out.println("\nTemperatura: " + temperaturaEntrada + " Volume: " + volumeEntrada);
-       fuzzy = new SistemaFuzzy();
-       fuzzy.criarVariavelLinguistica("Temperatura", 800, 1200);
-       fuzzy.criarVariavelLinguistica("Volume", 2, 12);
-       fuzzy.criarVariavelLinguistica("Pressao", 4, 12);
-       fuzzy.executarMecanismoInferencia(temperaturaEntrada, volumeEntrada);
-
-       temperaturaEntrada = 1122;
-       volumeEntrada = 5.2;
-       System.out.println("\nTemperatura: " + temperaturaEntrada + " Volume: " + volumeEntrada);
-       fuzzy = new SistemaFuzzy();
-       fuzzy.criarVariavelLinguistica("Temperatura", 800, 1200);
-       fuzzy.criarVariavelLinguistica("Volume", 2, 12);
-       fuzzy.criarVariavelLinguistica("Pressao", 4, 12);
-       fuzzy.executarMecanismoInferencia(temperaturaEntrada, volumeEntrada);
-    }
+//    public static void main(String[] args) {
+//
+//       double temperaturaEntrada = 965;
+//       double volumeEntrada = 11;
+//       System.out.println("Temperatura: " + temperaturaEntrada + " Volume: " + volumeEntrada);
+//       SistemaFuzzy fuzzy = new SistemaFuzzy();
+//       fuzzy.criarVariavelLinguistica("Temperatura", 800, 1200);
+//       fuzzy.criarVariavelLinguistica("Volume", 2, 12);
+//       fuzzy.criarVariavelLinguistica("Pressao", 4, 12);
+//       fuzzy.executarMecanismoInferencia(temperaturaEntrada, volumeEntrada);
+//
+//       temperaturaEntrada = 920;
+//       volumeEntrada = 7.6;
+//       System.out.println("\nTemperatura: " + temperaturaEntrada + " Volume: " + volumeEntrada);
+//       fuzzy = new SistemaFuzzy();
+//       fuzzy.criarVariavelLinguistica("Temperatura", 800, 1200);
+//       fuzzy.criarVariavelLinguistica("Volume", 2, 12);
+//       fuzzy.criarVariavelLinguistica("Pressao", 4, 12);
+//       fuzzy.executarMecanismoInferencia(temperaturaEntrada, volumeEntrada);
+//
+//       temperaturaEntrada = 1050;
+//       volumeEntrada = 6.3;
+//       System.out.println("\nTemperatura: " + temperaturaEntrada + " Volume: " + volumeEntrada);
+//       fuzzy = new SistemaFuzzy();
+//       fuzzy.criarVariavelLinguistica("Temperatura", 800, 1200);
+//       fuzzy.criarVariavelLinguistica("Volume", 2, 12);
+//       fuzzy.criarVariavelLinguistica("Pressao", 4, 12);
+//       fuzzy.executarMecanismoInferencia(temperaturaEntrada, volumeEntrada);
+//
+//       temperaturaEntrada = 843;
+//       volumeEntrada = 8.6;
+//       System.out.println("\nTemperatura: " + temperaturaEntrada + " Volume: " + volumeEntrada);
+//       fuzzy = new SistemaFuzzy();
+//       fuzzy.criarVariavelLinguistica("Temperatura", 800, 1200);
+//       fuzzy.criarVariavelLinguistica("Volume", 2, 12);
+//       fuzzy.criarVariavelLinguistica("Pressao", 4, 12);
+//       fuzzy.executarMecanismoInferencia(temperaturaEntrada, volumeEntrada);
+//
+//       temperaturaEntrada = 1122;
+//       volumeEntrada = 5.2;
+//       System.out.println("\nTemperatura: " + temperaturaEntrada + " Volume: " + volumeEntrada);
+//       fuzzy = new SistemaFuzzy();
+//       fuzzy.criarVariavelLinguistica("Temperatura", 800, 1200);
+//       fuzzy.criarVariavelLinguistica("Volume", 2, 12);
+//       fuzzy.criarVariavelLinguistica("Pressao", 4, 12);
+//       fuzzy.executarMecanismoInferencia(temperaturaEntrada, volumeEntrada);
+//    }
 
 }
