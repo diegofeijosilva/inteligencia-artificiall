45d09ad394fffbe63950b7a803852b309cf8cf66
gabriel.ecomp <gabriel.ecomp@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-08-09 13:58:57 +0000

    começando interface

---
+++ trunk/ProjetoFuzzy/src/ia/fuzzy/gui/PainelDeGraficos.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ia.fuzzy.gui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class PainelDeGraficos extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        double width = 210;
+        double height = 40;
+
+        float x = 50;
+        float y = 80;
+
+        Graphics2D g2d = (Graphics2D) g.create();
+
+        for (int i = 0; i < 9; i++) {
+            g2d.draw(new Rectangle2D.Double(x, y, width, height));
+            g2d.drawString((i+1)+"", x-30, y+25);
+            y += 50;
+        }
+        g2d.dispose();
+        
+    }
+
+
+}
