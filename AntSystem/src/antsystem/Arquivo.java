f2c5c0ad3c2145eebf9dff6cfbce4ac932bcf88a
lari.rsoares <lari.rsoares@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-08-19 20:25:21 +0000

    Programa da colonia de formigas

---
+++ AntSystem/src/antsystem/Arquivo.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package antsystem;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Larissa
+ */
+public class Arquivo {
+
+    public static final int LINHAS_ARQUIVO_DEFAULT = 2;
+
+    public double[][] lerArquivo( String arquivo, int colunas ) {
+        int linhas = LINHAS_ARQUIVO_DEFAULT;
+        double[][] dados = new double[linhas][colunas];
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(arquivo));
+            String str;
+            int count = 0;
+
+            while (in.ready() && count < linhas) {
+                str = in.readLine();
+                dados[count] = montarLinha(str, colunas);
+
+                count++;
+            }
+            in.close();
+
+            return dados;
+
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        return null;
+    }
+
+    private double[] montarLinha(String linhaString, int colunas) {
+        String[] partes = new String[colunas];
+        double[] resultado = new double[colunas];
+
+        partes = linhaString.split(" ");
+
+        for (int j = 0; j < colunas; j++) {
+            resultado[j] = Double.parseDouble(partes[j]);
+        }
+        return resultado;
+    }
+
+}
---
+++ AntSystem/src/antsystem/Cidade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package antsystem;
+
+/**
+ *
+ * @author Larissa
+ */
+public class Cidade {
+
+    //coordenadas
+    private int x;
+    private int y;
+
+    public Cidade(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
