02949ac78dc7213271c12c4650afaced6805450a
lari.rsoares <lari.rsoares@830c750b-5bb5-4650-f893-839a1b4f1ee6>
2011-08-21 18:32:47 +0000

    atualizando percusso otimo e tamanho do percusso otimo

--- AntSystem/src/antsystem/Feromonio.java
+++ AntSystem/src/antsystem/Feromonio.java
@@ -10,5 +10,17 @@ package antsystem;
  * @author Larissa
  */
 public class Feromonio {
+    
+    public static final double FEROMONIO_INICIAL_DEFAULT = 0.000001;
+    public double[][] trilhaDeFeromonio;
+
+    public void initTrilhaDeFeromonio(){
+        trilhaDeFeromonio = new double[AntSystem.QUANTIDADE_CIDADES_DEFAULT][AntSystem.QUANTIDADE_CIDADES_DEFAULT];
+        for(int i=0; i<AntSystem.QUANTIDADE_CIDADES_DEFAULT; i++){
+            for(int j=0; j<AntSystem.QUANTIDADE_CIDADES_DEFAULT; j++){
+                trilhaDeFeromonio[i][j] = FEROMONIO_INICIAL_DEFAULT;
+            }
+        }
+    }
 
 }
